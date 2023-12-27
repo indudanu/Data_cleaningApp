819bac2ba6c2055e8b5dd9400cb104f84cd7aa4b
indudanu <89530906+indudanu@users.noreply.github.com>
2023-12-27 08:54:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
-import fitz # imports the pymupdf library
-
+import fitz
+import tempfile
 # Function to convert PDF to text using pdfplumber
 def pdf_to_text(file_path):
     start_time = time.time()  # Record the start time
@@ -11,20 +11,25 @@ def pdf_to_text(file_path):
     end_time = time.time()  # Record the end time
     elapsed_time = end_time - start_time
     return text, elapsed_time
-
+def save_uploaded_file(uploaded_file):
+    _, temp_file_path = tempfile.mkstemp(suffix=".pdf")
+    with open(temp_file_path, "wb") as temp_file:
+        temp_file.write(uploaded_file.read())
+    return temp_file_path
 # Streamlit app
 def main():
     st.title("PDF Text Processing App")
 
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-
+    uploaded_file_path = save_uploaded_file(uploaded_file)
     if uploaded_file is not None:
-        text, elapsed_time = pdf_to_text(uploaded_file)
-
+        text, elapsed_time = pdf_to_text(uploaded_file_path)
+        st.subheader(f"Time to load PDF: {elapsed_time:.2f} seconds")
         st.subheader("Original Text:")
+        
         st.text(text)
 
-        st.subheader(f"Time to load PDF: {elapsed_time:.2f} seconds")
+       
 
 if __name__ == "__main__":
     main()
