92226665817b34c7522ca06035ba921fb31f48ee
indudanu <89530906+indudanu@users.noreply.github.com>
2023-12-27 08:39:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import streamlit as st
-import pdfplumber
 from langchain.document_loaders import PyMuPDFLoader
+import time
+
 # Function to convert PDF to text using pdfplumber
-"""def pdf_to_text(file_path):
-    with pdfplumber.open(file_path) as pdf:
-        text = ''
-        for page in pdf.pages:
-            text += page.extract_text()
-    return text"""
 def pdf_to_text(file_path):
     loader = PyMuPDFLoader(file_path)
+    start_time = time.time()  # Record the start time
     pages = loader.load()
-    return pages[0].page_content
+    end_time = time.time()  # Record the end time
+    elapsed_time = end_time - start_time
+    return pages[0].page_content, elapsed_time
+
 # Streamlit app
 def main():
     st.title("PDF Text Processing App")
@@ -19,10 +18,12 @@ def main():
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
     if uploaded_file is not None:
-        text = pdf_to_text(uploaded_file)
+        text, elapsed_time = pdf_to_text(uploaded_file)
 
         st.subheader("Original Text:")
         st.text(text)
+
+        st.subheader(f"Time to load PDF: {elapsed_time:.2f} seconds")
 
 if __name__ == "__main__":
     main()
